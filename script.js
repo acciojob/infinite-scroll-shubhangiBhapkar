10024d045056a58dd5e8d62bab0bed62e1ea1a3d
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-05 17:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here!
-
+const list = document.getElementById("infi-list");
+list.addEventListener("scroll", ()=> {
+	
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		for(int i=0;i<2;i++){
+			const ele = document.creatElement("li");
+			ele.textContent = "item";
+			list.appendChild(ele);
+		}
+		
+	}
+})
