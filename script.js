cab604af463caea321a2560031f1570048c81c72
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-05 17:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//your code here!
 const list = document.getElementById("infi-list");
-list.addEventListener("scroll", ()=> {
-	
-	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
-		for(int i=0;i<2;i++){
-			const ele = document.creatElement("li");
-			ele.textContent = "item";
-			list.appendChild(ele);
-		}
-		
-	}
-})
+
+list.addEventListener("scroll", () => {
+
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+
+        for (let i = 0; i < 2; i++) {
+            const ele = document.createElement("li");
+            ele.textContent = "New Item";
+            list.appendChild(ele);
+        }
+
+    }
+
+});
