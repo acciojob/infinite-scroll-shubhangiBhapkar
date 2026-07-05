dccb2031bc4c9767a15f7c0fd4de3ff4b0fa41cc
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-05 17:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const list = document.getElementById("infi-list");
-
+const count = list.children.length+1;
 list.addEventListener("scroll", () => {
 
     if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
 
         for (let i = 0; i < 2; i++) {
             const ele = document.createElement("li");
-            ele.textContent = "New Item";
+            ele.textContent = `item${count++}`;
             list.appendChild(ele);
         }
 
